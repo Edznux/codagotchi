1bd0e55ba8bdb912be1b64565b6d07dba63c0fb9
Edznux <edznux@gmail.com>
2021-03-28 00:47:14 +0100

    update hooks again

--- git-hooks.sh
+++ git-hooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build -t edznux/codagotchi .
+docker build -t codagotchi:latest .
 docker stop -f codagotchi
-docker run -d --rm -p 8080:8080 --name codagotchi edznux/codagotchi
+docker run -d --rm -p 8080:8080 --name codagotchi codagotchi:latest
