6a76856bb1a992fbfaf2050c76445a35ba3fa040
Edznux <edznux@gmail.com>
2021-03-28 00:51:53 +0100

    Add auto updating hooks

--- git-hooks.sh
+++ git-hooks.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+
 docker build -t codagotchi:latest .
 docker stop -f codagotchi
 docker run -d -p 8080:8080 -v $(pwd)/save.json:/app/save.json --name codagotchi codagotchi:latest
+
+# we "overwrite" ourselve in case the hook was updated
+./install-hooks.sh 
