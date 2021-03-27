ab7f4defb39899d503cbbb5b2b4b6091eedc029f
Edznux <edznux@gmail.com>
2021-03-28 00:49:07 +0100

    Add save volume

--- git-hooks.sh
+++ git-hooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build -t codagotchi:latest .
 docker stop -f codagotchi
-docker run -d --rm -p 8080:8080 --name codagotchi codagotchi:latest
+docker run -d -p 8080:8080 -v $(pwd)/save.json:/app/save.json --name codagotchi codagotchi:latest
