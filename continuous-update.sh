85e763acb60ede802e70ec3e6379cda127739e1d
Edznux <edznux@gmail.com>
2021-03-28 15:36:44 +0200

    Add version of docker containers

--- continuous-update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while true; do
-    date
-    git pull
-    sleep 60
-done
--- git-hooks.sh
+++ git-hooks.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+VERSION=$(git rev-parse --short HEAD)
 
-docker build -t codagotchi:latest .
+docker build -t codagotchi:$VERSION .
 docker stop codagotchi
 docker rm -f codagotchi
+
 docker run -d \
 --net host \
 -v $(pwd)/save.json:/app/save.json \
 --name codagotchi \
-codagotchi:latest
+codagotchi:$VERSION
 
 # we "overwrite" ourselve in case the hook was updated
 ./install-hooks.sh 
