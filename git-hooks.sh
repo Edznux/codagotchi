03d2e1e3a32b8d6e2e63b28d62f85963894eb24a
Edznux <edznux@gmail.com>
2021-03-28 04:11:23 +0200

    Trying things with docker

--- git-hooks.sh
+++ git-hooks.sh
@@ -2,8 +2,15 @@
 
 
 docker build -t codagotchi:latest .
-docker stop -f codagotchi
-docker run -d -p 8080:8080 -v $(pwd)/save.json:/app/save.json --name codagotchi codagotchi:latest
+docker stop codagotchi
+docker rm codagotchi
+docker run -d \
+-p 8080:8080 \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=${DISPLAY} \
+-v $(pwd)/save.json:/app/save.json \
+--name codagotchi \
+codagotchi:latest
 
 # we "overwrite" ourselve in case the hook was updated
 ./install-hooks.sh 
